185f982eeec120cefad19f0abe1cc37bd990ba0f
bonohbonoh <psmb2006@naver.com>
2024-03-17 18:18:18 +0900

    Initial commit

---
+++ src/main/java/com/leejeong/sagosiphomebackend/SagosipHomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leejeong.sagosiphomebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagosipHomeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagosipHomeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leejeong/sagosiphomebackend/SagosipHomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leejeong.sagosiphomebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagosipHomeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
